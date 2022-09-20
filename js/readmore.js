6c2a676030df05c7417fa4a34969467e4623830c
BestUserNameYouWillSee <77840705+BestUserNameYouWillSee@users.noreply.github.com>
2022-09-20 11:36:58 +0200

    sheeeesh2

--- js/readmore.js
+++
@@ -1,7 +0,0 @@
-function readmore() {
-    document.getElementById("myPopup").style.display = "block";
-  }
-  
-  function close() {
-    document.getElementById("myPopup").style.display = "none";
-  } 
