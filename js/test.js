b8715d1356fa7614b67ed44247e54a97a7eb36e0
BestUserNameYouWillSee <77840705+BestUserNameYouWillSee@users.noreply.github.com>
2022-11-07 14:11:51 +0100

    good luck

--- js/test.js
+++
@@ -1,9 +0,0 @@
-var request = new XMLHttpRequest();
-     
-request.open('GET', 'https://v2.jokeapi.dev/joke/Any?safe-mode');
-
-request.send();
-
-request.onload = () => {
-    console.log(request);
-}
