5e76804adbc07c433acebedaa593b1b2993b5e7d
dark045 <87452921+Dark045NL@users.noreply.github.com>
2022-11-08 14:26:31 +0100

    update

--- js/admin.js
+++ js/admin.js
@@ -1,26 +1,24 @@
-<<<<<<< HEAD
-    var request = new XMLHttpRequest();
- 
-    request.open('POST', 'https://api.bundeling.com/V2/login');
-     
-    request.setRequestHeader('Content-Type', 'application/json');
-    request.setRequestHeader('Api-Key', '830919aae3594240990576b77fd7e635');
-     
-    request.onreadystatechange = function(){
-       if(this.readyState === 4){
-            console.log('Status:', this.status);
-            console.log('Headers:', this.getAllResponseHeaders());
-            console.log('Body:', this.responseText);
-            authent = console.log('Body:', this.responseText);
-        }
-    };
-     
-    var body = {
-        "username": "roy.tieben@gmail.com",
-        "password": "zzqQAB%WZpgQ!jXR"
-    };
+var request = new XMLHttpRequest();
 
-    request.send(JSON.stringify(body));
 
-=======
->>>>>>> 936f626988c19bb1844063a367cc8da54716e149
+request.open('POST', 'https://api.bundeling.com/V2/login');
+
+
+request.setRequestHeader('Content-Type', 'application/json');
+request.setRequestHeader('Api-Key', '830919aae3594240990576b77fd7e635');
+
+
+request.onreadystatechange = function () {
+    if (this.readyState === 4) {
+        console.log('Status:', this.status);
+        console.log('Headers:', this.getAllResponseHeaders());
+        console.log('Body:', this.responseText);
+        localStorage.setItem('token', this.responseText.token);
+    }
+};
+
+var body = {
+    "username": "roy.tieben@gmail.com",
+    "password": "zzqQAB%WZpgQ!jXR"
+};
+request.send(JSON.stringify(body));
