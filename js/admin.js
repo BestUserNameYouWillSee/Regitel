fa1d5669c7fb7f8282c15a739dfe97db222853a3
dark045 <87452921+Dark045NL@users.noreply.github.com>
2022-11-09 12:35:35 +0100

    update API

--- js/admin.js
+++ js/admin.js
@@ -1,19 +1,17 @@
-var request = new XMLHttpRequest();
 
+
+var request = new XMLHttpRequest();
 
 request.open('POST', 'https://api.bundeling.com/V2/login');
 
-
 request.setRequestHeader('Content-Type', 'application/json');
 request.setRequestHeader('Api-Key', '830919aae3594240990576b77fd7e635');
-
 
 request.onreadystatechange = function () {
     if (this.readyState === 4) {
         console.log('Status:', this.status);
         console.log('Headers:', this.getAllResponseHeaders());
         console.log('Body:', this.responseText);
-        localStorage.setItem('token', this.responseText.token);
     }
 };
 
@@ -21,4 +19,13 @@ var body = {
     "username": "roy.tieben@gmail.com",
     "password": "zzqQAB%WZpgQ!jXR"
 };
+
 request.send(JSON.stringify(body));
+
+const token = localStorage.getItem('token');
+if (token) {
+    request.setRequestHeader('Authorization', `BundelingAuth ${token}`);
+}
+
+const id = localStorage.getItem('token');
+console.log(id);
