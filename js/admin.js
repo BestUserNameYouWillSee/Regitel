0ddb0cad0a277dd3ab13ebd51ce55ef36fbc49a7
MrFlashed <53785391+MrFlashed@users.noreply.github.com>
2022-11-11 13:08:49 +0100

    fff

--- js/admin.js
+++ js/admin.js
@@ -1,5 +1,3 @@
-
-
 var request = new XMLHttpRequest();
 
 request.open('POST', 'https://api.bundeling.com/V2/login');
@@ -14,7 +12,6 @@ request.onreadystatechange = function () {
         console.log('Body:', this.responseText);
     }
 };
-
 var body = {
     "username": "roy.tieben@gmail.com",
     "password": "zzqQAB%WZpgQ!jXR"
@@ -23,9 +20,21 @@ var body = {
 request.send(JSON.stringify(body));
 
 const token = localStorage.getItem('token');
-if (token) {
-    request.setRequestHeader('Authorization', `BundelingAuth ${token}`);
-}
 
-const id = localStorage.getItem('token');
-console.log(id);
+var request = new XMLHttpRequest();
+ 
+request.open('GET', 'https://api.bundeling.com/V2/news');
+ 
+request.setRequestHeader('Content-Type', 'application/json');
+request.setRequestHeader('Api-Key', '830919aae3594240990576b77fd7e635');
+request.setRequestHeader('Authorization', `BundelingAuth ${token}`);
+ 
+request.onreadystatechange = function(){
+    if(this.readyState === 4){
+        console.log('Status:', this.status);
+        console.log('Headers:', this.getAllResponseHeaders());
+        console.log('Body:', this.responseText);
+    }
+};
+ 
+request.send();
--- js/news.js
+++ js/news.js
@@ -1,25 +1,24 @@
 
 
-var request = new XMLHttpRequest();
+//var request = new XMLHttpRequest();
 
-request.open('POST', 'https://api.bundeling.com/V2/login');
+//request.open('POST', 'https://api.bundeling.com/V2/login');
 
-request.setRequestHeader('Content-Type', 'application/json', 'Api-Key', '830919aae3594240990576b77fd7e635');
-request.setRequestHeader('Api-Key', '830919aae3594240990576b77fd7e635');
+//request.setRequestHeader('Content-Type', 'application/json', 'Api-Key', '830919aae3594240990576b77fd7e635');
+//request.setRequestHeader('Api-Key', '830919aae3594240990576b77fd7e635');
 
 
-request.onreadystatechange = function () {
-    if (this.readyState === 4) {
-        console.log('Status:', this.status);
-        console.log('Headers:', this.getAllResponseHeaders());
-        console.log('Body:', this.responseText);
-    }
-};
+//request.onreadystatechange = function () {
+///    if (this.readyState === 4) {
+//        console.log('Status:', this.status);
+//        console.log('Headers:', this.getAllResponseHeaders());
+//        console.log('Body:', this.responseText);
+//    }
+//};
 
-var body = {
-    "username": "roy.tieben@gmail.com",
-    "password": "zzqQAB%WZpgQ!jXR"
-};
+///var body = {
+//    "username": "roy.tieben@gmail.com",
+//    "password": "zzqQAB%WZpgQ!jXR"
+//};
 
-request.send(JSON.stringify(body));
-
+//request.send(JSON.stringify(body));
