7e681e0d7ba26876630a6fd0faa30c2134c27571
MrFlashed <53785391+MrFlashed@users.noreply.github.com>
2022-11-14 08:37:00 +0100

    rrrrrraawr
    rrrr

--- js/admin.js
+++ js/admin.js
@@ -1,40 +1,36 @@
-var request = new XMLHttpRequest();
+function news(token) {
+    var xhr = new XMLHttpRequest();
+    
+    xhr.addEventListener("readystatechange", function() {
+      if(this.readyState === 4) {
+        console.log(this.responseText);
+      }
+    });
+    
+    xhr.open("GET", "https://api.bundeling.com/V2/news");
+xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.setRequestHeader("Api-Key", "830919aae3594240990576b77fd7e635");
+    xhr.setRequestHeader("Authorization", `BundelingAuth ${token}`);
+    
+    xhr.send();
+}
 
-request.open('POST', 'https://api.bundeling.com/V2/login');
+var data = JSON.stringify({
+  "username": "roy.tieben@gmail.com",
+  "password": "zzqQAB%WZpgQ!jXR"
+});
 
-request.setRequestHeader('Content-Type', 'application/json');
-request.setRequestHeader('Api-Key', '830919aae3594240990576b77fd7e635');
+var xhr = new XMLHttpRequest();
 
-request.onreadystatechange = function () {
-    if (this.readyState === 4) {
-        console.log('Status:', this.status);
-        console.log('Headers:', this.getAllResponseHeaders());
-        console.log('Body:', this.responseText);
-    }
-};
-var body = {
-    "username": "roy.tieben@gmail.com",
-    "password": "zzqQAB%WZpgQ!jXR"
-};
+xhr.addEventListener("readystatechange", function() {
+  if(this.readyState === 4) {
+    const data = JSON.parse(this.responseText);
+      news(data.token);
+  }
+});
 
-request.send(JSON.stringify(body));
+xhr.open("POST", "https://api.bundeling.com/V2/login");
+xhr.setRequestHeader("Content-Type", "application/json");
+xhr.setRequestHeader("Api-Key", "830919aae3594240990576b77fd7e635");
 
-const token = localStorage.getItem('token');
-
-var request = new XMLHttpRequest();
- 
-request.open('GET', 'https://api.bundeling.com/V2/news');
- 
-request.setRequestHeader('Content-Type', 'application/json');
-request.setRequestHeader('Api-Key', '830919aae3594240990576b77fd7e635');
-request.setRequestHeader('Authorization', `BundelingAuth ${token}`);
- 
-request.onreadystatechange = function(){
-    if(this.readyState === 4){
-        console.log('Status:', this.status);
-        console.log('Headers:', this.getAllResponseHeaders());
-        console.log('Body:', this.responseText);
-    }
-};
- 
-request.send();
+xhr.send(data);
