6367727b80a0f69dd034a71cf83f4098d2813e89
MrFlashed <roy.tieben@gmail.com>
2022-12-12 11:35:03 +0100

    api image arrya

--- js/community-users.js
+++ js/community-users.js
@@ -1,28 +1,10 @@
 function users(token) {
     var xhr = new XMLHttpRequest();
 
-    xhr.addEventListener("readystatechange", function () {
-        if (this.readyState === 4) {
-            const names1 = [""];
-            const Images = [""];
-            let i = 0;
-            
-
-            console.log(JSON.parse(this.responseText));
-            while (i < 50) {
-                Images.push(JSON.parse(this.responseText).data[i].picture);
-                names1.push(JSON.parse(this.responseText).data[i].name + "<br/>");
-               // document.getElementById("api3").innerHTML =  names1.join("");
-
-                document.getElementById("image").src =  Images.join("");
-                document.getElementById("naam").innerHTML =  names1.join("");
-                i += 1;
-
-            }
-
-        }
-    });
-
+    xhr.open("GET", "https://api.bundeling.com/V2/files");
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.setRequestHeader("Api-Key", "830919aae3594240990576b77fd7e635");
+    xhr.setRequestHeader("Authorization", `BundelingAuth ${token}`);
 
     xhr.open("GET", "https://api.bundeling.com/V2/users");
     xhr.setRequestHeader("Content-Type", "application/json");
@@ -30,6 +12,26 @@ function users(token) {
     xhr.setRequestHeader("Authorization", `BundelingAuth ${token}`);
 
     xhr.send();
+
+    xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === 4) {
+            const names1 = [""];
+            const Images = [""];
+            //const newRequest = new Request("https://api.bundeling.com/V2/users", Images);
+            let i = 0;
+            console.log(JSON.parse(this.responseText))
+            console.log(Images);
+            while (i < 50) {
+                Images.push(JSON.parse(this.responseText).data[i].picture);
+                //const ImagesTest = [JSON.parse(this.responseText).data[i].picture];
+                names1.push(JSON.parse(this.responseText).data[i].name + "<br/>");
+                //document.getElementById("image").src = ImagesTest;
+                document.getElementById("image").src =  Images.join("https://api.bundeling.com/V2/users");
+                document.getElementById("naam").innerHTML =  names1.join("");
+                i += 1;
+            }
+        }
+    });
 }
 
 var data = JSON.stringify({
